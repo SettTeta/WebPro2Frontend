fb62cba2719d575d2e935091559a76b299fd2d28
Sett Teta <stetanonsakul@gmail.com>
2023-03-24 14:16:28 +0700

    transfer his html code and imported bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import { SSRProvider } from 'react-bootstrap';
+import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function App({ Component, pageProps }) {
+  
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js")
+  }, [])
+  
+  return (
+    <SSRProvider>
+      <Component {...pageProps} />
+    </SSRProvider>
+  )
 }
+
+export default App
